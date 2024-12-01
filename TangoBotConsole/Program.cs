71942d676d844221a4252e0bd54c7fa6e1d7bfe8
JUSTO DISLA <jdisla@gmail.com>
2024-12-01 12:16:52 -0400

    Add new TangoBotConsole project to solution
    Introduce a new console application project named `TangoBotConsole`:
    - Add `Program.cs` with a `Main` method printing "Hello, World!".
    - Create `TangoBotConsole.csproj` with .NET 8.0 configurations.
    - Update `TangoBotSolution.sln` to include the new project.

---
+++ TangoBotConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TangoBotConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
